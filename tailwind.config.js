324a3f195fc755958423d9131aaff806dbb8cd33
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-12-02 15:16:58 -0700

    fixed glow-color in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,9 +9,20 @@ export default {
     extend: {},
   },
   plugins: [
-    typography,
-    plugin(function ({ addVariant }) {
-      addVariant("glow", ".glow-capture .glow-overlay &");
-    }),
-  ],
-};
+  plugin(
+    function ({ addVariant }) {
+      addVariant("glow", ".glow-capture .glow-overlay &")
+    },
+    {
+      theme: {
+        extend: {
+          colors: {
+            // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-mix
+            glow: "color-mix(in srgb, var(--glow-color) calc(<alpha-value> * 100%), transparent)",
+          },
+        },
+      },
+    }
+  ),
+],
+}
