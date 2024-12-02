c433758272a9b14227d7b8779c737b4145c18149
Nik Efimov <efimov.nikolai.ig@gmail.com>
2024-12-02 13:48:07 -0700

    added initial config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+
+const plugin = require("tailwindcss/plugin")
+
 export default {
-  content: [],
+  content: ["index.html"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/typography"),
+    plugin(
+      function ({ addVariant }) {
+        addVariant("glow", ".glow-capture .glow-overlay &")
+      },
+      {
+        theme: {
+          extend: {
+            colors: {
+              // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-mix
+              glow: "color-mix(in srgb, var(--glow-color) calc(<alpha-value> * 100%), transparent)",
+            },
+          },
+        },
+      }
+    ),
+  ],
 }
-
