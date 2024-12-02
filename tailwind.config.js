8bf1311ecee25c07219211302a34c7765643c3f2
Nik Efimov <43499639+efimovnik@users.noreply.github.com>
2024-12-02 14:30:28 -0700

    added tailwind demo code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
-const plugin = require("tailwindcss/plugin")
+import plugin from "tailwindcss/plugin";
+import typography from "@tailwindcss/typography";
 
 export default {
   content: ["index.html"],
@@ -8,21 +9,9 @@ export default {
     extend: {},
   },
   plugins: [
-    require("@tailwindcss/typography"),
-    plugin(
-      function ({ addVariant }) {
-        addVariant("glow", ".glow-capture .glow-overlay &")
-      },
-      {
-        theme: {
-          extend: {
-            colors: {
-              // https://developer.mozilla.org/en-US/docs/Web/CSS/color_value/color-mix
-              glow: "color-mix(in srgb, var(--glow-color) calc(<alpha-value> * 100%), transparent)",
-            },
-          },
-        },
-      }
-    ),
+    typography,
+    plugin(function ({ addVariant }) {
+      addVariant("glow", ".glow-capture .glow-overlay &");
+    }),
   ],
-}
+};
